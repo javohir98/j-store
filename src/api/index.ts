6b152ac1172e8a4cab33b849514b66c6d8e95337
javohir98 <47714181+javohir98@users.noreply.github.com>
2025-08-15 21:03:23 +0500

    feat: add axios api setup with interceptors

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+});
+
+api.interceptors.request.use((config) => {
+  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
+
+export default api;
+export { api };
