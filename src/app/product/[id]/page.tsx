9206ac3f8d46e8aed1271752dad936a60ed48cb8
javohir98 <47714181+javohir98@users.noreply.github.com>
2025-08-15 20:43:42 +0500

    feat: add reusable product list and new card design

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-white">
       This is a product detail page.
     </div>
   );
